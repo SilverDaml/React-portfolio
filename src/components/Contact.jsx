c4ed3f233195cec3a1893f0993e61d99f34a88fd
Francisco Arellano <SilverD.amld@gmail.com>
2024-02-01 13:50:40 +0000

    Portfolio address update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,16 +37,15 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/search?q=La+plata+Buenos+aires&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d152515.6931206764!2d-6.245704099999999!3d53.324320099999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e80ea27ac2f%3A0xa00c7a9973171a0!2sDubl%C3%ADn%2C%20Irlanda!5e0!3m2!1ses!2ses!4v1706794950165!5m2!1ses!2ses" referrerpolicy="no-referrer-when-downgrade"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
+                
               </h2>
               <p className="mt-1">
-                607 num 939 <br />
-                La Plata, Buenos Aires
+                
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -59,7 +58,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">221-4198776</p>
+              <p className="leading-relaxed">0894551272</p>
             </div>
           </div>
         </div>
